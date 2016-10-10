799f07e59a65cfd4b18407b0afe59f2c3eea4d6b
helemork <helemork@gmail.com>
2016-10-10 15:14:29 +0200

    mergeconflictfix and some tiny header css stuffs

--- artsdata/src/components/header/Header.js
+++ artsdata/src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import MenuButton from '../buttons/MenuButton'
+import "./header.css"
 
 class Header extends Component {
   render() {
     return(
-      <div>
+      <div className="header">
         <h1>OVERSKRIFT OOT OOT OOT</h1>
         <MenuButton btnText="Knapp 1" />
         <MenuButton btnText="Knapp 2" />
