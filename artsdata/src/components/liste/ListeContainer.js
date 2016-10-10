b5507e67df0f560e66b67337a358b973240917c3
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-10 16:27:17 +0200

    Loading data from a server dynamically

--- artsdata/src/components/liste/ListeContainer.js
+++ artsdata/src/components/liste/ListeContainer.js
@@ -6,15 +6,31 @@ class ListeContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      names: ['Bjørn', 'Ole', 'Nora']
+      data: {'Observations': []}
     }
+    this.fetchHandler()
+  }
+
+  fetchHandler() {
+    fetch('http://artskart2.artsdatabanken.no/api/observations/list?Taxons=31113,77987&pageSize=50', {
+      method: 'GET'
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      this.setState(Object.assign({}, self.state, { data: data }));
+    })
+    .catch((error) => {
+      this.setState(Object.assign({}, self.state, { error: error }));
+    });
   }
 
   render() {
     var rows = []
-    for (var i = 0; i < 3; i++) {
-      rows.push(<ListeElement name={this.state.names[i]} />)
-    }
+    for (var i = 0; i < this.state.data['Observations'].length; i++)
+      rows.push(<ListeElement name={ this.state.data['Observations'][i]['Name'] } key={ i } />)
+    
     return(
       <div>
         <ListeSearch />
