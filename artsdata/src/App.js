15791629533932ee92fcd80a2d206e3176ba83b1
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-10 14:42:26 +0200

    Forgot to change the App component

--- artsdata/src/App.js
+++ artsdata/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ContentContainer from './components/content_container/ContentContainer'
+import ContentContainer from './components/content/ContentContainer'
 
 class App extends Component {
   render() {
