fa0319ac84bcbe169e5a580a31c1ece5176e45f1
helemork <helemork@gmail.com>
2016-10-10 14:50:06 +0200

    added header yep!

--- artsdata/src/components/content/ContentContainer.js
+++ artsdata/src/components/content/ContentContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import ListeContainer from '../liste/ListeContainer'
+import Header from '../header/Header'
 
 class ContentContainer extends Component {
   render() {
     return(
       <div>
+        <Header />
         <ListeContainer />
       </div>
     );
