b4d1b425013d60d6c5b0fa6bb748cbaf52d93347
Andreas Haugan Aursand <Andreas-MAC@dhcp-10-22-41-7.wlan.ntnu.no>
2016-10-19 13:03:38 +0200

    fixed ugly whitespace (i'm a real pro)

--- artsdata-react/src/components/content/ContentContainer.js
+++ artsdata-react/src/components/content/ContentContainer.js
@@ -4,33 +4,33 @@ import KartContainer from '../kart/KartContainer'
 import Header from '../header/Header'
 
 class ContentContainer extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            toggle: true,
-            toggleContainer: <ListeContainer />
-        }
+  constructor (props) {
+    super(props)
+    this.state = {
+      toggle: true,
+      toggleContainer: <ListeContainer />
     }
-    toggleEvent () {
-        if (this.state.toggle) {
-            this.setState({
-                toggle: false,
-                toggleContainer: <KartContainer />
-            })
-        } else {
-            this.setState({
-                toggle: true,
-                toggleContainer: <ListeContainer />
-            })
-        }
+  }
+  toggleEvent () {
+    if (this.state.toggle) {
+      this.setState({
+        toggle: false,
+        toggleContainer: <KartContainer />
+      })
+    } else {
+      this.setState({
+        toggle: true,
+        toggleContainer: <ListeContainer />
+      })
     }
+  }
 
-    render () {
-        return (
-          <div>
-            <Header toggleHandler={this.toggleEvent.bind(this)} />
-            {this.state.toggleContainer}
-          </div>
+  render () {
+    return (
+      <div>
+        <Header toggleHandler={this.toggleEvent.bind(this)} />
+        {this.state.toggleContainer}
+      </div>
     )
   }
 }
