9b5febaceaaf3d67229ca83321bf2324b49a2499
Niklas Molnes Hole <nikasmh@hotmail.com>
2016-10-28 16:35:37 +0200

    Purge the semicolonsgit add server.js !

--- artsdata-react/server.js
+++ artsdata-react/server.js
@@ -3,28 +3,28 @@ var app = express()
 var bodyParser = require("body-parser")
 var os = require("os")
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 app.use('/dist', express.static(__dirname + '/dist'))
 app.use('/node_modules', express.static(__dirname + '/node_modules'))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   "extended": true
-}));
+}))
 
 app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
 // API routes
-var router = express.Router();
+var router = express.Router()
 
 // Test route
 router.get("/", function(req, res) {
   res.json({
     "error": false,
     "message": "This is the artsdata API"
-  });
-});
+  })
+})
 
 // Register API routes
 app.use('/api', router)
@@ -34,7 +34,7 @@ app.use('/api', router)
 // same port.
 if (os.hostname() != 'it2810-04') {
   app.listen(3000, function() {
-    console.log('Artsdata app listening on port 3000');
+    console.log('Artsdata app listening on port 3000')
   })
 } else
   app.listen(80)
