170dd0fe7d2037fda61bfcc8c7097d254d3e900b
T0llef <havard.tollefsen@gmail.com>
2016-11-02 16:26:45 +0100

    begynne på store

--- artsdata-react/src/components/liste/ListeContainer.js
+++ artsdata-react/src/components/liste/ListeContainer.js
@@ -31,7 +31,6 @@ class ListeContainer extends Component {
       sort: '',
       counties: []
     }
-    this.fetchHandler()
   }
 
   /**
---
+++ artsdata-react/src/store/api-data.service.js
@@ -0,0 +1,14 @@
+const state = [
+  {
+    id: 1,
+    data: {'Observations': []},
+    searchFilter: {
+      name: '',
+      county: [],
+      municipality: '',
+      locality: ''
+    },
+    sort: '',
+    counties: []
+  }
+]
