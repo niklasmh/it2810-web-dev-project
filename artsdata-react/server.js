c5391ff11e3f8270d2c933ebc1da6e0adc61b4b5
Andreas Haugan Aursand <Andreas-MAC@dhcp-10-22-41-7.wlan.ntnu.no>
2016-11-02 11:37:04 +0100

    fixed merge-c

--- artsdata-react/server.js
+++ artsdata-react/server.js
@@ -1,14 +1,16 @@
 var express = require('express')
 var app = express()
-var bodyParser = require("body-parser")
-var os = require("os")
+var bodyParser = require('body-parser')
+var os = require('os')
+var io = require('socket.io')
+var MongoClient = require('mongodb').MongoClient, assert = require('assert')
 
 app.use(express.static(__dirname))
 app.use('/dist', express.static(__dirname + '/dist'))
 app.use('/node_modules', express.static(__dirname + '/node_modules'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  "extended": true
+  'extended': true
 }))
 
 app.get('/*', function (req, res) {
@@ -19,10 +21,10 @@ app.get('/*', function (req, res) {
 var router = express.Router()
 
 // Test route
-router.get("/", function(req, res) {
+router.get('/', function (req, res) {
   res.json({
-    "error": false,
-    "message": "This is the artsdata API"
+    'error': false,
+    'message': 'This is the artsdata API'
   })
 })
 
@@ -32,9 +34,31 @@ app.use('/api', router)
 // Start the server. But first check if it is online. Else we want to run a devserver at port 3000.
 // If we had the possibility to change the nginx og apache config, then we could have used the
 // same port.
-if (os.hostname() != 'it2810-04') {
-  app.listen(3000, function() {
+if (os.hostname() !== 'it2810-04') {
+  app.listen(3000, function () {
     console.log('Artsdata app listening on port 3000')
   })
-} else
+} else {
   app.listen(80)
+}
+
+// MongoDB functions for calling and recieveing from the database
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject'
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function (err, db) {
+  assert.equal(null, err)
+  console.log('Connected successfully to server')
+
+  var insertDocuments = function (db, callback) {
+  // Get the documents collection
+    var collection = db.collection('artsdata')
+  // Insert some documents
+    collection.insertMany([], function (err, result) {
+      assert.equal(null, err)
+    })
+  }
+  //  db.close() - for use to close the database
+})
